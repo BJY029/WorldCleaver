8c27162eb76fa89b70b781c041c39ff3c41f5ff7
BJY029 <go333dragon@gmail.com>
2024-10-31 02:51:11 +0900

    item algorithim
    아이템 리스트에 랜덤 아이템이 표시되도록 추가

---
+++ WorldCleaverProject/Assets/Codes/DisplayItems.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//해당 스크립트를 각 버튼에 삽입한다.
+public class DisplayItems : SingleTon<DisplayItems>
+{ 
+    //해당 버튼에 소속된 icon, itemname, desc를 삽입한다.
+    public Image itemIcon;
+    public Text itemText;
+    public Text itemDescription;
+    private Item assignedItem;
+
+    //ItemManager에서 해당 함수를 호출해서 해당 버튼의 정보들을 초기화한다.
+    public void SetItem(Item item)
+    {
+        assignedItem = item;
+        itemIcon.sprite = item.icon;
+        itemText.text = item.itemName;
+        itemDescription.text = item.description;
+    }
+
+    //각 Button의 OnClick 함수에 삽입되며, 클릭될 시, 플레이어의 아이템 리스트에 삽입되고
+    //해당 버튼의 부모의 부모(panel)의 크기를 0으로 만든다.
+    public void OnItemSelected()
+    {
+        ItemManager.Instance.AddItemToPlayerInventory(assignedItem);
+
+        if (transform.parent != null && transform.parent.parent != null)
+        {
+            transform.parent.parent.localScale = Vector3.zero;
+        }
+    }
+}
--- WorldCleaverProject/Assets/Codes/ItemManager.cs
+++ WorldCleaverProject/Assets/Codes/ItemManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //아이템 정보를 저장하는 item 클래스
 public class Item
@@ -20,8 +21,10 @@ public class Item
 
 public class ItemManager : SingleTon<ItemManager>
 {
-    //모든 아이템을 저장하는 리스트
-    public List<Item> allItems;
+    //버튼 3개
+	public List<Button> itemButtons;
+	//모든 아이템을 저장하는 리스트
+	public List<Item> allItems;
     //플레이어가 지니고 있는 아이템 리스트
     public List<Item> playerItems;
     //적 플레이어가 지니고 있는 아이템 리스트
@@ -48,11 +51,28 @@ public class ItemManager : SingleTon<ItemManager>
         allItems = new List<Item>
         {
             //해당 아이템 정보들을 생성
-            new Item("Potion", "Heals 100 pw", potionIcon)
+            new Item("Potion", "Heals 100 pw", potionIcon),
+            new Item("Deer", "Give random damage", deerIcon),
+            new Item("Eagel", "Take one Item from enemy", eagleIcon),
+            new Item("Fight", "Play minigame to charge mana", fightIcon),
+            new Item("Flare", "Acquire additional items", flareIcon),
+            new Item("Ginseng", "Recovery mana", ginsengIcon),
+            new Item("Honey", "Restores the health of the villagers", honeyIcon),
+            new Item("Oil", "Lower my attack damage", OilIcon),
+            new Item("Sap", "Heals tree Health", sapIcon),
+            new Item("Smoke", "Invisible tree's health", sapIcon),
+            new Item("Squid", "Lower one's opponent's attack damage", squidIcon),
+            new Item("TreeShild", "Lower one's opponent's attack damage", treeShildIcon),
+            new Item("Velvet", "Significant increase in damage to trees", velvetIcon)
         };
 
+        //플레이어의 아이템들을 저장하는 리스트
         playerItems = new List<Item>();
+        //적의 아이템들을 저장하는 리스트
         enemyItems = new List<Item>();
+
+        //게임 시작시, 해당 버튼 선택 창의 버튼 리스트를 랜덤 아이템으로 초기화 할 수 있도록 함수를 호출한다.
+        SetRandomItemsOnButtons();
     }
 
     //랜덤 아이템을 반환하는 함수
@@ -61,5 +81,37 @@ public class ItemManager : SingleTon<ItemManager>
     {
         int randomIndex = Random.Range(0, allItems.Count);
         return allItems[randomIndex];
+    }
+
+    //아이템버튼 리스트를 중복없게 할당하는 함수
+    public void SetRandomItemsOnButtons()
+    {
+        //버튼 아이템 리스트
+        List<Item> choseanItems = new List<Item>();
+
+        //아이템 버튼 개수 만큼 반복한다(3번)
+        for(int i = 0; i < itemButtons.Count; i++)
+        {
+            //임시 아이템 버퍼
+            Item randomItem;
+            do
+            {   
+                //랜덤 함수 인덱스를 뽑아서 저장한다
+                randomItem = GiveRandomItem();
+            }while(choseanItems.Contains(randomItem)); //만약 리스트 안에 이미 해당 아이템이 있으면 다시 반복문 실행
+            //해당 아이템을 버튼 아이템 리스트에 추가한다.
+            choseanItems.Add(randomItem);
+
+            //해당 버튼 인덱스에 저장된 아이템을 DisplayItem의 set함수에게 넘겨서 표시한다.
+            DisplayItems display = itemButtons[i].GetComponent<DisplayItems>();
+            display.SetItem(randomItem);
+        }
+    }
+
+    //플레이어가 선택한 아이템을 플레이어 리스트에 추가하는 함수
+    public void AddItemToPlayerInventory(Item item)
+    {
+        playerItems.Add(item);
+        Debug.Log($"{item.itemName} 아이템을 인벤토리에 추가했습니다!");
     }
 }
